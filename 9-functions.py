64bbb74a909b3372722ef2d1283e1a1bdb6b3596
Afsal Gaffar <afsal.gaffar@gmail.com>
2025-07-07 01:26:53 +0200

    update

---
+++ 9-functions.py
@@ -0,0 +1,23 @@
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
+def sum(num1=0, num2=0):
+    # print(num1+num2)
+    if (type(num1) is not int or type(num2) is not int):
+        return
+    return num1+ num2
+
+# sum(2,3)
+# sum(1,7)
+# sum(100,3)
+
+total = sum(7,2)
+print(total)
+
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+multiple_items("Dave", "John", "Sara")
